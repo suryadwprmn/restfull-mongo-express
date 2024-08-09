763128ce70597e696515400ca2823d8e34a3fbbe
suryadwprmn <suryadwipramana18@gmail.com>
2024-08-09 11:11:53 +0700

    Restfull

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const port = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+
+app.listen(port, () => {
+    console.log(`app listening on port http://localhost:${port}`)
+});
+
+mongoose.connect('mongodb://localhost:27017/shop_db')
+.then((result) => {
+    console.log('Terhubung ke Database MongoDB');   
+}).catch((err) => {
+    console.error(err);
+});
+
+app.get('', (req,res) => {
+    res.render('home')
+});
