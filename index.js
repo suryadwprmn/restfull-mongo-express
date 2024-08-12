107af98ecfc3839fac7d3d7264d3b8fae5c3ead1
suryadwprmn <suryadwipramana18@gmail.com>
2024-08-12 14:49:55 +0700

    berhasil restfull crud

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require("mongoose");
 const app = express();
 
@@ -12,6 +13,9 @@ app.set('view engine', 'ejs');
 
 // Middleware untuk mengurai data formulir
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware untuk method-override
+app.use(methodOverride('_method'));
 
 /* Model */
 const Product  = require('./model/product');
@@ -62,7 +66,41 @@ app.get('/products/:id' ,async(req, res) => {
     res.render('product/show', {product});
 })
 
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.status(404).send('Product not found');
+        }
+        res.render('product/edit', { product });
+    } catch (error) {
+        console.error('Error finding product:', error);
+    }
+});
 
+app.put('/products/:id', async (req,res) => {
+    const {id} = req.params;
+    try {
+        const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true})
+        res.redirect(`/products/${product._id}`);
+    } catch (error) {
+        console.error('Error updating data', error)
+    }
+})
+
+app.delete('/products/:id', async(req,res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).send('Product not found');
+        }
+        res.redirect('/products')
+    } catch (error) {
+        console.error('Error deleting product: ', error)
+    }
+})
 
 
 
